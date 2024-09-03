6478e36409a0a83cce94f30692807c154173489a
vaibhav-dawane <vaibhavdawane07@gmail.com>
2024-09-03 23:56:38 +0530

    tried doing containerizing application

--- Backend/dbConfig.js
+++ Backend/dbConfig.js
@@ -3,12 +3,14 @@ const { Client } = pkg;
 
 // defining parameters to connect with postgres
 const client = new Client({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'newdb',
-    password: 'vaibhav',
-    port: 5432
+    user:  process.env.POSTGRES_USER || 'postgres',
+    host: process.env.POSTGRES_HOST || 'pg-db',
+    database: process.env.POSTGRES_DB || 'newdb',
+    password: process.env.POSTGRES_PASSWORD || 'vaibhav',
+    port: process.env.POSTGRES_PORT || 5432
 })
+
+// docker run -p 5432:5432 -d --name pg-db -e POSTGRES_PASSWORD=vaibhav --network mern-nw postgres
 
 client.connect()
   .then(() => console.log('Connected to PostgreSQL'))
